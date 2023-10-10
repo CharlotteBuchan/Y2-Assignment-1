6b764a375c371f3bce0f918d47d51d173f782b42
CharlotteBuchan <530240@chrome.escg.ac.uk>
2023-10-10 14:42:30 +0100

    Triangle Types and Rectangle Perimeter Starters

---
+++ Rectangle Perimeter/Rectangle Perimeter/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Rectangle_Perimeter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float w;
+            float l;
+            float p;
+
+            Console.WriteLine("Enter the width of your rectangle: ");
+            w = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the length of your rectangle: ");
+            l = float.Parse(Console.ReadLine());
+
+            p = ((w + l) * 2);
+            Console.WriteLine("Your perimeter is "+p);
+        }
+    }
+}
