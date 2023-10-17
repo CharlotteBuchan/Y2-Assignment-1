97781f5f8ca7126b823b754c5992bd4ec4d6920c
CharlotteBuchan <530240@chrome.escg.ac.uk>
2023-10-17 15:22:40 +0100

    Game Theories Starter

---
+++ Game Theories starter/Game Theories starter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static int SumOfAbsoluteValues(int[] numbers)
+    {
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += Math.Abs(number);
+        }
+        return sum;
+    }
+
+    static void Main(string[] args)
+    {
+        int[] numbers = { -5, 10, -3, 8, 2 };
+        int result = SumOfAbsoluteValues(numbers);
+        Console.WriteLine("Sum of absolute values: " + result);
+    }
+}
